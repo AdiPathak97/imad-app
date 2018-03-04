f14e38265b492bd2f2da3c4979b7876112c7f49a
AdiPathak97 <36631927+AdiPathak97@users.noreply.github.com>
2018-03-04 15:56:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artcile-one', function (req, res) {
+    res.send('Article one requested');
+});
+
+app.get('/artcile-two', function (req, res) {
+    res.send('Article two requested');
+});
+
+app.get('/artcile-three', function (req, res) {
+    res.send('Article three requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
