ac21d985e5cbe2c642a3cc0e3547807af3a5df8c
AdiPathak97 <36631927+AdiPathak97@users.noreply.github.com>
2018-03-04 18:12:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 //change text of main element div
 var element = document.getElementById('main_text');
 element.innerHTML = "New value from IMAD console";
+//move image
+var img = document.getElementById("img");
+img.onclick =  function() {
+   img.style.marginLeft = '100px' ; 
+};
