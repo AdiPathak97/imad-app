78016e0e136224f0bed202c8a4685fb4e322ca8f
AdiPathak97 <36631927+AdiPathak97@users.noreply.github.com>
2018-03-04 18:20:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ var element = document.getElementById('main_text');
 element.innerHTML = "New value from IMAD console";
 //move image
 var img = document.getElementById("madi");
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft  + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick =  function() {
-   img.style.marginLeft = '100px' ; 
+    var interval = setInterval(moveRight, 100);
 };
