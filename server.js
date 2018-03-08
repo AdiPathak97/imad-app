19d7869fae5caf586822a07e56385361ff6ba18f
AdiPathak97 <36631927+AdiPathak97@users.noreply.github.com>
2018-03-08 20:34:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,9 +47,8 @@ function createTemplate(data) {
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
-    var htmlTemplate = 
-        `
-        <!DOCTYPE HTML>
+    
+    var htmlTemplate = `
         <html>
             <head>
                 <title>
@@ -76,8 +75,6 @@ function createTemplate(data) {
                 </div>
             </body>
         </html>
-
-
         `;
         return htmlTemplate;
 }  
@@ -92,6 +89,17 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function(req, res) { // URL: /submit-name?name=xxxx
+    // Get name from request
+    var name = req.query.name;
+    
+    names.push(name);
+    // JSON : Javascript Object Notation
+    res.send(JSON.stringify(names)); 
+});
+
+
 app.get('/:articleName', function (req, res) {
     // articleName == article-one
     // articles[articleName] == {} content object for article one 
@@ -103,23 +111,12 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names = [];
-app.get('/submit-name', function(req, res) { // URL: /submit-name?name=xxxx
-    // Get name from request
-    var name = req.query.name;
-    
-    names.push(name);
-    // JSON : Javascript Object Notation
-    res.send(JSON.stringify(names)); 
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
